994c27ea96eb19c4f8b0e46306e256e1ecb7eec7
Chepman32 <antonkerch555@gmail.com>
2022-02-12 11:54:08 +0300

    Autumn

--- assets/Images.js
+++ assets/Images.js
@@ -1,5 +1,5 @@
 const getSeason = d => Math.floor((d.getMonth() / 12 * 4)) % 4
-const season = ['Winter', 'Spring', 'Summer', 'Autumn'][getSeason(new Date())]
+const season = 'Autumn'
 
 function dependsImages() {
     switch(season) {
@@ -23,6 +23,26 @@ function dependsImages() {
                     bird2: require('./img/bird2.png'),
                     bird3: require('./img/bird3.png'),
                 }
+                case "Summer":
+                    return {
+                        background: require(`./img/background-summer.png`),
+                        floor: require('./img/floor.png'),
+                        pipeCore: require('./img/pipe_core.png'),
+                        pipeTop: require('./img/pipe_top.png'),
+                        bird1: require('./img/bird1.png'),
+                        bird2: require('./img/bird2.png'),
+                        bird3: require('./img/bird3.png'),
+                    }
+                    case "Autumn":
+                        return {
+                            background: require(`./img/background-autumn.png`),
+                            floor: require('./img/floor.png'),
+                            pipeCore: require('./img/pipe_core.png'),
+                            pipeTop: require('./img/pipe_top.png'),
+                            bird1: require('./img/bird1.png'),
+                            bird2: require('./img/bird2.png'),
+                            bird3: require('./img/bird3.png'),
+                        }
     }
 }
 const  set = dependsImages()
